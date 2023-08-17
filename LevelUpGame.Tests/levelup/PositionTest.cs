9899ec91f712e6aca8d0946c43ffca14618a30e9
vangv <vue.vang@gmail.com>
2023-08-17 17:14:08 +0000

    add Position changes

---
+++ LevelUpGame.Tests/levelup/PositionTest.cs
@@ -0,0 +1,24 @@
+
+using levelup.cli;
+using NUnit.Framework;
+
+namespace levelup {
+    [TestFixture]
+    public class PositionTest {
+        private Position _position;
+        private readonly  int XCOORDINATES = 0;
+        private readonly  int YCOORDINATES = 0;
+
+        [SetUp]
+        public void SetUp() {
+            _position = new Position(XCOORDINATES, YCOORDINATES);
+        }
+
+        [Test]
+        public void TestPositionHasXCoordinates() {
+            Assert.AreEqual(XCOORDINATES,_position.XCoordinates);
+        }
+    
+    }
+
+}
---
+++ LevelUpGame/levelup/cli/Position.cs
@@ -0,0 +1,27 @@
+using System.Drawing;
+
+namespace levelup.cli {
+    public class Position {
+
+        private readonly int _xCoordinates;
+        private readonly int _yCoordinates;
+        private readonly Point _coordinates;
+
+        public int XCoordinates => _xCoordinates;
+        public Point Coordinates => _coordinates;
+
+        public Position(){
+
+        }
+
+        public Position(int xCoordinates, int yCoordinates){
+            _xCoordinates = xCoordinates;
+            _yCoordinates = yCoordinates;
+        }
+
+
+
+        
+
+    }
+}
