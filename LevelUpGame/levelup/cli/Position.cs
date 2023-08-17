6ec9a7c613114f48ae5783e44422338361ec727e
vangv <vue.vang@gmail.com>
2023-08-17 19:43:08 +0000

    correct coordinate logic

--- LevelUpGame/levelup/cli/Position.cs
+++ LevelUpGame/levelup/cli/Position.cs
@@ -3,22 +3,22 @@ using System.Drawing;
 namespace levelup.cli {
     public class Position {
 
-        private readonly int _xCoordinates = 0;
-        private readonly int _yCoordinates = 0;
-        private readonly Point _coordinates;
+        private int _xCoordinates = 0;
+        private int _yCoordinates = 0;
+        private Point _coordinates;
 
         public int XCoordinates => _xCoordinates;
         public int YCoordinates => _yCoordinates;
         public Point Coordinates => _coordinates;
-        
 
         public Position(){
-
+            _coordinates = new Point(_xCoordinates, _yCoordinates);        
         }
 
         public Position(int xCoordinates, int yCoordinates){
             _xCoordinates = xCoordinates;
             _yCoordinates = yCoordinates;
+            _coordinates = new Point(xCoordinates, yCoordinates);
         }
 
 
